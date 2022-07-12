59bd430e91827efad446e929431d453a94262bca
Matthew Fournier <matthew.fournier@pointercreative.com>
2022-07-12 15:19:59 -0400

    script 3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import minimist from 'minimist'
 import { terminal } from 'terminal-kit'
 import test1 from './scripts/test1'
 import test2 from './scripts/test2'
+import test3 from './scripts/test3'
 
 (() => {
   const args = minimist(process.argv.slice(2))
@@ -9,7 +10,8 @@ import test2 from './scripts/test2'
 
   const validScripts = [
     test1,
-    test2
+    test2,
+    test3
   ]
 
   const scriptToRun = validScripts.find(
