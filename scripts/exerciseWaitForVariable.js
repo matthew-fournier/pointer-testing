8a35e11b9b122a56020678752c9595e7292df31e
Matthew Fournier <matthew.fournier@pointercreative.com>
2022-08-03 17:04:18 -0400

    answer in

--- scripts/exerciseWaitForVariable.js
+++ scripts/exerciseWaitForVariable.js
@@ -10,15 +10,29 @@ const defineVariableOnTimeOut = (timeoutTime) => {
 }
 
 const waitForVariablePromise = (maxWaitTime) => {
-  // ...
+  return new Promise((resolve, reject) => {
+    let timeWaited = 0
+
+    const waitInterval = setInterval(() => {
+      timeWaited += 100
+      console.log(`${timeWaited}ms`)
+
+      if (timeWaited < maxWaitTime && typeof waitForMe === 'undefined') { return }
+      clearInterval(waitInterval)
+
+      typeof waitForMe === 'undefined'
+        ? reject(new Error(`waitForMe variable tool longer than maxWaitTime of ${maxWaitTime}ms`))
+        : resolve(waitForMe)
+    }, 100)
+  })
 }
 
-const exerciseWaitForVariable = () => {
+const exerciseWaitForVariable = async () => {
   try {
     scriptTitle('Wait for variable < waitForMe > to be defined')
 
-    defineVariableOnTimeOut(900)
-    waitForVariablePromise(5000)
+    defineVariableOnTimeOut(1000)
+    await waitForVariablePromise(5000)
 
     terminal.magenta('The Value of waitForMe is: ').yellow(`${waitForMe}\n\n`)
   } catch (err) {
@@ -32,87 +46,4 @@ export default exerciseWaitForVariable
   NOTE:
   Wait for variable to be difined before proceeding
   Should have a max timeout
-*/
-
-/*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-
----------------------------------------------------------
-
-await waitForVariablePromise(5000)
-
----------------------------------------------------------
-
-return new Promise((resolve, reject) => {
-  let timeWaited = 0
-
-  const waitInterval = setInterval(() => {
-    timeWaited += 100
-    console.log(`${timeWaited}ms`)
-
-    if (timeWaited < maxWaitTime && typeof waitForMe === 'undefined') { return }
-    clearInterval(waitInterval)
-
-    typeof waitForMe === 'undefined'
-      ? reject(new Error(`waitForMe variable tool longer than maxWaitTime of ${maxWaitTime}ms`))
-      : resolve(waitForMe)
-  }, 100)
-})
-
----------------------------------------------------------
-
 */
