2f95f489e70b9e223b6e3cbd67fc0ad1a159585a
Matthew Fournier <matthew.fournier@pointercreative.com>
2022-08-30 16:50:33 -0400

    working

--- compiledTypescript/basicTypescript.js
+++ compiledTypescript/basicTypescript.js
@@ -1,11 +1,46 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import scriptTitle from '../helpers/scriptTitle';
-const onlyAcceptsNumbers = (passedNumber) => {
-    return passedNumber;
+// This function only accepts a numbers and a string. The return value will be of type string
+const onlyAcceptsNumberAndString = (passedNumber, passedString) => {
+    return `My number is < ${passedNumber} > and my string is < "${passedString}" >`;
 };
-const basicTypescript = () => {
-    scriptTitle('Exercise to minimize the code needed to complete the FizzBuzz challenge');
-    console.log('eeef');
-    onlyAcceptsNumbers(2);
-};
+const basicTypescript = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        scriptTitle('Basic TypeScript');
+        // myNumber must have a value with type number
+        const myNumber = 2;
+        // valueFromFunction must have a value of type string
+        const valueFromFunction = onlyAcceptsNumberAndString(myNumber, 'Hi');
+        console.log(valueFromFunction);
+        // My status can only equal a value defined in the "DevStatus" type
+        const myStatus = 'Approved';
+        // myTaskList will be an array of object that follow the DevTask Interface
+        const myTaskList = [
+            {
+                title: 'Task1',
+                dueDate: new Date('September 22, 2022'),
+                status: 'In Progress'
+            }
+        ];
+        // The new pushed object must follow the DevTask Interface
+        myTaskList.push({
+            title: 'Task1',
+            dueDate: new Date('September 24, 2022'),
+            status: 'Not Started'
+        });
+        console.log(myTaskList, "d");
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
 export default basicTypescript;
 //# sourceMappingURL=basicTypescript.js.map
--- typescripts/basicTypescript.ts
+++ typescripts/basicTypescript.ts
@@ -1,16 +1,59 @@
 import scriptTitle from '../helpers/scriptTitle'
 
+// Declare a custom type "DevStatus"
+type DevStatus = 'Not Started' | 'In Progress' | 'Approved' | 'Complete'
 
-
-const onlyAcceptsNumbers = (passedNumber: number) => {
-  return passedNumber
+// Sets interfect for devStatus objects
+interface DevTask {
+  title: string,
+  dueDate: Date,
+  status: DevStatus
 }
 
-const basicTypescript = () => {
-  scriptTitle('Exercise to minimize the code needed to complete the FizzBuzz challenge')
-  console.log('eeef')
-  onlyAcceptsNumbers(2)
+// This function only accepts a numbers and a string. The return value will be of type string
+const onlyAcceptsNumberAndString = (passedNumber: number, passedString: string) : string => {
+  return `My number is < ${passedNumber} > and my string is < "${passedString}" >`
+}
 
+const basicTypescript = async () => {
+  try {
+    scriptTitle('Basic TypeScript')
+
+    // myNumber must have a value with type number
+    const myNumber: number = 2
+  
+  
+    // valueFromFunction must have a value of type string
+    const valueFromFunction: string = onlyAcceptsNumberAndString(myNumber, 'Hi')
+    console.log(valueFromFunction)
+  
+    
+    // My status can only equal a value defined in the "DevStatus" type
+    const myStatus: DevStatus = 'Approved'
+  
+    // myTaskList will be an array of object that follow the DevTask Interface
+    const myTaskList: DevTask[] = [
+      {
+        title: 'Task1',
+        dueDate: new Date('September 22, 2022'),
+        status: 'In Progress'
+      }
+    ]
+    // The new pushed object must follow the DevTask Interface
+    myTaskList.push({
+      title: 'Task1',
+      dueDate: new Date('September 24, 2022'),
+      status: 'Not Started'
+    })
+  
+  
+  
+    console.log(myTaskList, "d")
+
+
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export default basicTypescript
