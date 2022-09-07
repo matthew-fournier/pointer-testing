1984f71737805a93ad6550789e5b98c0c0337198
Matthew Fournier <matthew.fournier@pointercreative.com>
2022-09-07 12:43:56 -0400

    little shopify product example

---
+++ typescripts/axiosExample.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse } from 'axios'
+import scriptTitle from '../helpers/scriptTitle'
+
+interface ShopifyProduct {
+  id: number
+  title: string
+  variants: Array<{
+    title: string
+    price: string
+    sku: string
+  }>
+}
+
+const axiosExample = async (): Promise<void> => {
+  try {
+    scriptTitle('Axios Example')
+
+    const axiosRes: AxiosResponse = await axios('https://dudeproducts.com/products/dude-wipes-48ct-dispenser-30pk-travel-home-away-combo.json')
+    const productData: ShopifyProduct = axiosRes.data.product
+
+    const { title, id, variants: [{ title: firstVarTitle, price, sku }] } = productData
+    console.log(title, id, firstVarTitle, price, sku)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export default axiosExample
