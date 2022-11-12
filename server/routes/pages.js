35be04adc3e3253592828adb22963a485f7212fd
Matthew Fournier <matthew.fournier@pointercreative.com>
2022-11-11 23:11:21 -0500

    chatbox

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -1,5 +1,4 @@
 import Router from 'koa-router'
-import glob from 'glob-promise'
 import * as fs from 'fs'
 
 const router = new Router({ prefix: '/pages' })
@@ -10,14 +9,19 @@ const pages = fs.readdirSync('web/pages')
 pages.forEach(async (page) => {
   const pageName = page.split('.')[0]
 
-  const documentBody = fs.readFileSync(`web/pages/${page}`, 'utf8')
+  let documentBody = fs.readFileSync(`web/pages/${page}`, 'utf8')
+  const componentInstances = [...documentBody.matchAll(/\[\[(.*?)\]\]/g)]
+
+  componentInstances.forEach((componentInstance) => {
+    documentBody = documentBody.replace(
+      componentInstance[0],
+      fs.readFileSync(`web/components/${componentInstance[1]}.html`, 'utf8')
+    )
+  })
 
   const document = fs.readFileSync('web/layout/document.html', 'utf8')
     .replace('[[title]]', pageName.toUpperCase())
     .replace('[[body]]', documentBody)
-
-  // const componentList = await glob('web/components/**/**.html')
-  // console.log(g)
 
   router.get(`/${pageName === 'home' ? '' : pageName}`, async (ctx) => {
     ctx.response.set('content-type', 'txt/html')
