50508ca5d6473cff4b497f842baaeb2b759a32dc
Matthew Fournier <matthew.fournier@pointercreative.com>
2022-11-14 13:30:20 -0500

    random

--- server/sockets.js
+++ server/sockets.js
@@ -1,4 +1,5 @@
 import { Server } from 'socket.io'
+import { randomList } from '../helpers/randList'
 
 let io = null
 
@@ -12,17 +13,44 @@ const startSockets = async (httpServer) => {
     }
   })
 
+  let isMessageSent = false
+
   io.on('connection', (socket) => {
     socket.emit('connectionReturn', socket.id)
 
     socket.on('messageSent', (messageData) => {
       io.emit('updateMessages', messageData)
+
+      if (!isMessageSent) {
+        startRandSend()
+        isMessageSent = true
+      }
     })
   })
 
   console.log(`Websocket Running on port:${process.env.SERVER_PORT}`)
 
   return io
+}
+
+const startRandSend = () => {
+  let currentIndex = 0
+
+  const sendFact = () => {
+    io.emit('updateMessages', {
+      message: randomList[currentIndex],
+      username: 'I am C^l3b?',
+      userID: '--',
+      type: 'basic'
+    })
+
+    currentIndex = currentIndex + 1 < randomList.length
+      ? currentIndex + 1
+      : 0
+  }
+
+  sendFact()
+  setInterval(() => sendFact(), 1000 * 30)
 }
 
 export default startSockets
