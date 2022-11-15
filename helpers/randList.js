699eb9a61d9dbffc6de6d43fd972ad17b27d52c7
Matthew Fournier <matthew.fournier@pointercreative.com>
2022-11-15 11:46:58 -0500

    sockets

--- helpers/randList.js
+++ helpers/randList.js
@@ -1,7 +1,38 @@
-export const randomList = [
+const randUsername = 'I am P0!NT3R'
+
+const randomList = [
   'Can anyone here help me get back to slack?',
-  'Or at the very least Google Meet?',
   'I\'m so confused...',
-  'Okay, seriously, what\'s going on?!',
+  'Okay seriously, what\'s going on?!',
   'Figured it out, bye!'
 ]
+
+export const randEnteredChat = (io) => {
+  io.emit('updateMessages', {
+    message: `${randUsername} has entered the chat 👋`,
+    username: randUsername,
+    userID: '--',
+    type: 'welcome'
+  })
+}
+
+export const startRandSend = (io) => {
+  let currentIndex = 0
+
+  const sendFact = (loopInterval) => {
+    io.emit('updateMessages', {
+      message: randomList[currentIndex],
+      username: randUsername,
+      userID: '--',
+      type: 'basic'
+    })
+
+    currentIndex++
+    if (currentIndex >= randomList.length) {
+      clearInterval(loopInterval)
+    }
+  }
+
+  sendFact()
+  const loopInterval = setInterval(() => sendFact(loopInterval), 1000 * 5)
+}
--- server/sockets.js
+++ server/sockets.js
@@ -1,5 +1,5 @@
 import { Server } from 'socket.io'
-import { randomList } from '../helpers/randList'
+import { randEnteredChat, startRandSend } from '../helpers/randList'
 
 let io = null
 
@@ -23,8 +23,8 @@ const startSockets = async (httpServer) => {
 
       if (!isMessageSent && messageData.type === 'basic') {
         isMessageSent = true
-        setTimeout(() => randEnteredChat(), 1000 * 5)
-        setTimeout(() => startRandSend(), 1000 * 15)
+        setTimeout(() => randEnteredChat(io), 1000 * 5)
+        setTimeout(() => startRandSend(io), 1000 * 15)
       }
     })
   })
@@ -32,38 +32,6 @@ const startSockets = async (httpServer) => {
   console.log(`Websocket Running on port:${process.env.SERVER_PORT}`)
 
   return io
-}
-
-const randUserName = 'I am P0!NT3R'
-
-const randEnteredChat = () => {
-  io.emit('updateMessages', {
-    message: `${randUserName} has entered the chat 👋`,
-    username: randUserName,
-    userID: '--',
-    type: 'welcome'
-  })
-}
-
-const startRandSend = () => {
-  let currentIndex = 0
-
-  const sendFact = (loopInterval) => {
-    io.emit('updateMessages', {
-      message: randomList[currentIndex],
-      username: randUserName,
-      userID: '--',
-      type: 'basic'
-    })
-
-    currentIndex++
-    if (currentIndex >= randomList.length) {
-      clearInterval(loopInterval)
-    }
-  }
-
-  sendFact()
-  const loopInterval = setInterval(() => sendFact(loopInterval), 1000 * 5)
 }
 
 export default startSockets
